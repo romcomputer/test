7bc6c30d15f0f964a3b9aea204435de40a01520a
Krishna Bhalala <krishna.bhalala@silentinfotech.com>
2019-12-04 14:23:05 +0530

    Added init files

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': u'Rom Computer',
+    'version': '1.0',
+    'category': '',
+    'description': u"""
+
+""",
+    'author': u'Rom Computer',
+    'depends': ['base'
+                ],
+
+    'data': [
+        ],
+
+    'qweb': [
+    ],
+
+    'application': True,
+    'license': u'LGPL-3',
+    'installable': True,
+}
+
